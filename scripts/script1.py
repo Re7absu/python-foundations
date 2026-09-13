ce5b210a1494ad4c26ec9f787be545b149fe7be8
Re7ab <re7ab.su95@gmail.com>
2026-09-13 05:08:25 +0300

    Refactor scripts using comprehensions

---
+++ scripts/script1.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Any
+
+# comprehensions
+students: list[dict[str, Any]] = [
+    {"name": "Ali", "grade": 85},
+    {"name": "Sara", "grade": 92},
+    {"name": "Rehab", "grade": 78},
+    {"name": "Omar", "grade": 88},
+]
+passed_students = [student["name"] for student in students if student["grade"] >= 80]
+print(passed_students)
+
+
+@dataclass
+class Student:
+    name: str
+    grade: float
+
+    def display(self) -> str:
+        return f"{self.name}: {self.grade}"
+
+
+student = Student("Rehab", 95)
+
+print(student.display())
